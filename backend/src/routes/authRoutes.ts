cede94f8fef4d61d5b20fc04a81b6865c0799843
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-04-30 20:48:48 -0300

    feat: rotas de cadastro e login com JWT

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import { AppDataSource } from '../database'
 import { User } from '../entities/User'
 import hash from 'bcryptjs'
 
-export async function userRoutes(app: FastifyInstance) {
+export async function authRoutes(app: FastifyInstance) {
   app.post('/users', async (request, reply) => {
     const { name, email, password } = request.body as any
     const userRepository = AppDataSource.getRepository(User)
@@ -18,4 +18,29 @@ export async function userRoutes(app: FastifyInstance) {
     await userRepository.save(user)
     return reply.status(201).send({ message: "usuario criado" })
   })
+
+
+  app.post('/login', async (request, reply) => {
+    const { email, password } = request.body as any
+    const userRepository = AppDataSource.getRepository(User)
+    const user = await userRepository.findOneBy({ email })
+
+    if (!user) {
+      return reply.status(401).send({ message: "Email incorreto" })
+    }
+
+    const passwordMatch = await hash.compare(password, user.password)
+
+    if (!passwordMatch) {
+      return reply.status(401).send({ message: "Senha incorreta" })
+    }
+
+    const token = app.jwt.sign(
+      { name: user.name }, 
+      { sub: String(user.id), expiresIn: '7d' }
+    )
+
+    return { token }
+    
+})
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,25 @@
 import Fastify from 'fastify'
 import { AppDataSource } from './database'
+import fastifyJwt from '@fastify/jwt'
+import { authRoutes } from './routes/authRoutes'
 
 const app = Fastify()
 
+app.register(fastifyJwt, {
+    secret: 'asdjasdjakjdlaksjdlasd'
+})
+
+app.register(authRoutes)
+
 const start = async () => {
     try {
-        await AppDataSource.initialize()
-        console.log("banco conectado")
-
-        await app.listen({ port: 3333 })
-        console.log("servidor rodando")
-    } 
-    catch (err) {
-        console.error("nao conectou, erro: ", err)
-        process.exit(1)
-    }
+    await AppDataSource.initialize()
+    await app.listen({ port: 3333 })
+    console.log("rodando")
+  }
+   catch (err) {
+    console.error(err)
+  }
 }
 
 start()
