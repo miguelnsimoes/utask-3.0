30b9ea4e172fe5e17919ae7e1f733425f18c3a41
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-04-30 14:07:35 -0300

    feat: criação das entidade Card

---
+++ backend/src/entities/Card.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm"
+import { User } from "./User"
+
+@Entity('cards')
+export class Card {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column({ type: 'varchar' })
+    title!: string
+
+    @Column({ type: 'text', nullable: true }) 
+    description!: string
+
+    @Column({ type: 'varchar', default: 'todo' })
+    column!: string
+
+    @ManyToOne(() => User, (user) => user.id)
+    user!: User
+}
