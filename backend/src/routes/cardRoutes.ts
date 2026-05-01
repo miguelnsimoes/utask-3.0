3083731057f3cd3f55976e9a0db413f4ad52edca
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-01 17:55:06 -0300

    add rota put card

--- backend/src/routes/cardRoutes.ts
+++ backend/src/routes/cardRoutes.ts
@@ -32,4 +32,31 @@ export async function cardRoutes(app: FastifyInstance){
         })
     })
 
+    app.put('/cards/:id', async(request, reply)=>{
+        const {id} = request.params as {id: string}
+        const {title, description, column} = request.body as any
+        const userId = request.user.sub
+        const cardRepository = AppDataSource.getRepository(Card)
+
+        const card = await cardRepository.findOneBy({
+            id: Number(id),
+            user: {id: Number(userId)}
+        })  
+
+        if(!card){
+            return reply.status(404).send({message: "Card não encontrado"})
+        }
+
+        card.title = title ?? card.title
+        card.description = description ?? card.description
+        card.column = column ?? card.column
+
+        await cardRepository.save(card)
+        return card
+
+        
+
+
+    })
+
 }
