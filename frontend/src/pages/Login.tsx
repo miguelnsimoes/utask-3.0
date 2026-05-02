5895fe280a82eb4e8fa0000a2e296a9635b4b55c
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-02 11:18:20 -0300

    add fontes e cores, terminando parte direita do login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,29 +1,36 @@
 export function Login() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen font-poppins">
       
       <div className="w-1/2 bg-gray-100">
       </div>
 
-      <div className="w-1/2 flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold text-blue-600 mb-8">uTask 3.0</h1>
+      <div className="w-1/2 flex flex-col items-center justify-center px-20">
+        <h1 className="text-5xl font-bold text-blue-600 mb-12">uTask 3.0</h1>
         
-        <div className="flex flex-col w-80 mb-4">
+        <div className="flex flex-col w-full max-w-md">
           <label className="text-sm mb-1">E-mail</label>
           <input 
             type="email"
             placeholder="Endereço de e-mail"
-            className="border border-gray-300 rounded-md px-4 py-2 outline-none focus:border-blue-500 mb-4"
+            className="border border-gray-300 rounded-lg px-4 py-3 mb-4 outline-none bg-blue-50 focus:border-blue-500"
           />
           <label className="text-sm mb-1">Senha</label>
           <input 
-            type="senha"
+            type="password"
             placeholder="Senha secreta"
-            className="border border-gray-300 rounded-md px-4 py-2 outline-none focus:border-blue-500"
+            className="border border-gray-300 rounded-lg px-4 py-3 outline-none bg-blue-50 focus:border-blue-500"
           />
+
+          <a href="#" className="text-sm text-blue-600 mt-2 mb-6 underline">Esqueceu a senha?</a>
+
+          <button className="bg-blue-600 text-white py-3 rounded-full font-semibold hover:bg-blue-700 transition">Entrar</button>
           
+            <hr className="my-8 w-32 mx-auto border-1 border-gray-400" />
+            <p className="text-center text-sm underline">Não tem cadastro  ? Crie uma conta</p>
         </div>
-      </div>
+
+      </div>  
 
     </div>
   )
