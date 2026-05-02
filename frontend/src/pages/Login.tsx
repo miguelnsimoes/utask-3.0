993e28de18c3f054d83af4192d0c4ffaae3c700b
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-02 19:27:47 -0300

    feat: add lista com cores do figma, terminando tela login, add feature ocultar senha

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,9 @@
 import { Header } from '../components/Header'
 import illustration from '../assets/Ilustração do login.png'
+import { useState } from 'react'
 
 export function Login() {
+  const [showPassword, setShowPassword] = useState(false)
   return (
   <div className="flex flex-col h-screen">
     <Header />
@@ -12,30 +14,39 @@ export function Login() {
           <img src={illustration} alt="ilustração" className="w-3/4" />
        </div>
 
-      <div className='w-px h-[601px] bg-blue-300 self-center'></div>
+      <div className='w-px bg-primary my-16'></div>
 
       <div className="w-1/2 flex flex-col items-center justify-start pt-22 px-20">
-        <h1 className="w-[205px] h-[63px] text-5xl font-bold text-[#226ED8] mb-12">uTask 3.0</h1>
+        <h1 className="text-5xl font-bold text-primary-dark mb-12">uTask 3.0</h1>
         
         <div className="flex flex-col w-full max-w-md">
           <label className="text-sm mb-1 font-poppins font-normal">E-mail</label>
           <input 
             type="email"
             placeholder="Endereço de e-mail"
-            className="border rounded-lg px-4 py-3 mb-4 outline-none bg-blue-50 focus:border-blue-500"
+            className="border rounded-lg px-4 py-3 mb-4 outline-none bg-blue-50 focus:border-primary"
           />
-          <label className="text-sm mb-1 font-poppins font-normal">Senha</label>
+        <label className="text-sm mb-1 font-poppins font-normal">Senha</label>
+        <div className='relative'>
           <input 
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             placeholder="Senha secreta"
-            className="border rounded-lg px-4 py-3 outline-none bg-blue-50 focus:border-blue-500"
+            className="border rounded-lg px-4 py-3 outline-none bg-blue-50 focus:border-primary w-full"
           />
+          <button 
+          type='button'
+          onClick={()=> setShowPassword(!showPassword)}
+          className="absolute right-3 top-3 text-400">
+          <span className='material-icons'>
+             {showPassword ? 'visibility' : 'visibility_off'}
+          </span>   
+          </button>
+        </div>
+          <a href="#" className="text-sm text-primary-dark mt-2 mb-6 underline font-poppins font-normal">Esqueceu a senha?</a>
 
-          <a href="#" className="text-sm text-blue-600 mt-2 mb-6 underline font-poppins font-normal">Esqueceu a senha?</a>
-
-          <button className="bg-blue-600 text-white py-3 rounded-full font-semibold hover:bg-blue-700 transition">Entrar</button>
+          <button className="bg-primary-dark text-white py-3 rounded-full font-semibold hover:bg-primary-darker transition">Entrar</button>
           
-            <hr className="my-8 w-32 mx-auto border-1 border-gray-400" />
+            <hr className="my-8 w-50 mx-auto border-1 border-gray-400" />
             <p className="text-center text-sm underline font-poppins font-normal">Não tem cadastro  ? Crie uma conta</p>
         </div>
 
