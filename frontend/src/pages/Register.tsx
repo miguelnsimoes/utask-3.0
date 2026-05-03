5d5cb36f8b5d5f758230fb8719ca6945048df8ea
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-03 01:30:56 -0300

    ajustando posicao dos elemntos

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -6,11 +6,20 @@ export function Register(){
         <div className="flex flex-col h-screen">
             <Header />
 
-            <div className='flex flex-1 justify-end'>
+            <div className='flex flex-1'>
+                <div className="w-1/2 flex items-start justify-center">
+                    <div className="w-1/2 flex flex-col items-start justify-start pt-20 px-10">
+                        <h1 className="text-5xl font-bold text-primary-dark mb-12">uTask 3.0</h1>
+                    </div>
+                </div>
+
+                <div className="w-px bg-primary h-3/4 self-center"></div>
+
                 <div className="w-1/2 flex items-center justify-center">
                     <img src={illustration} alt="ilustração" className="w-3/4" />
                 </div>
+
             </div>
         </div>
     )
-}
+}   
