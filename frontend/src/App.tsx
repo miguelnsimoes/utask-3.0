586c8b645ce7c9db4b242b1ce8c1e2a67484b0aa
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-04 13:31:10 -0300

    add routes para paginacao de login e cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-//import { Login } from './pages/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './pages/Login'
 import { Register } from './pages/Register'
 
 function App() {
-  //return <Login />
-  return <Register />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
