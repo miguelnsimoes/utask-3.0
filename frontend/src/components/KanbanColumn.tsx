df23726bc7f237cb216d8ead13bb60d67fa0dc35
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-09 14:46:16 -0300

    estruturacao fundo dos board para cards

---
+++ frontend/src/components/KanbanColumn.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    title: string
+}
+
+export function KanbanColumn({ title }: Props) {
+    return (
+        <div className="flex flex-col flex-1 bg-[#EEEEEE] p-4 rounded-x1 shadow-sm">
+            Teste
+        </div>
+    )
+}
--- frontend/src/pages/Kanban.tsx
+++ frontend/src/pages/Kanban.tsx
@@ -1,11 +1,17 @@
 import { KanbanHeader } from '../components/KanbanHeader'
 import { QuoteCard } from '../components/QuoteCard'
+import { KanbanColumn } from '../components/KanbanColumn'
 
 export function Kanban() {
     return (
         <div className="flex flex-col h-screen bg-gray-50">
             <KanbanHeader />
             <QuoteCard />
+            <div className="flex flex-1 gap-6 px-40 py-20"> 
+                <KanbanColumn title="A fazer" />
+                <KanbanColumn title="Em andamento" />
+                <KanbanColumn title="Feito" />
+            </div>
         </div>
     )
 }
