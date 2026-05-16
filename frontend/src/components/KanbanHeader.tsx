d15523d5327920ee7da51490b23c7508bf221dae
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-16 00:00:47 -0300

    feat: header modo dark

--- frontend/src/components/KanbanHeader.tsx
+++ frontend/src/components/KanbanHeader.tsx
@@ -1,5 +1,5 @@
 import type { Dispatch, SetStateAction } from 'react'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo azul.svg'
 import lightMode from '../assets/light_mode.svg'
 import darkModeIcon from '../assets/dark_mode.svg'
 
@@ -10,17 +10,17 @@ interface Props {
 
 export function KanbanHeader({ darkMode, setDarkMode }: Props) {
     return (
-        <header className="w-full bg-primary-dark flex items-center h-20 relative px-10 shadow-sm">
+        <header className={`w-full flex items-center h-20 relative px-10 shadow-sm ${darkMode ? 'bg-primary-mid-dark' : 'bg-light-bg'}`}>
             
             <img src={logo} alt="uTask logo" className="w-60 h-8" />
 
-            <h1 className="text-white text-2xl font-bold absolute left-1/2 -translate-x-1/2">uTask 3.0</h1>
+            <h1 className="text-2xl font-bold absolute left-1/2 -translate-x-1/2 text-primary-blue">uTask 3.0</h1>
 
             <button
                 onClick={() => setDarkMode(!darkMode)}
                 className={`ml-auto mr-6 w-14 h-7 rounded-full flex items-center px-1 transition-all duration-300 ${
                     darkMode
-                        ? 'bg-gradient-to-r from-[#222222] to-[#2E2E2E]' 
+                        ? 'bg-gradient-to-r from-dark-surface to-dark-deep'
                         : 'bg-yellow-400'
                 }`}>
                 <div className={`w-6 h-6 bg-white rounded-full flex items-center justify-center transition-transform duration-300 ${
