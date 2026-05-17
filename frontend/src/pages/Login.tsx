0ac3126bb5551674a9570cf112a508cc2de15f9b
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-05-17 14:13:59 -0300

    feat: ajustar padding e espaçamento no mobile

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -66,14 +66,14 @@ export function Login() {
       
       <div className="flex flex-1">  
 
-       <div className="hidden sm:flex w-1/2 items-center justify-center"> 
+       <div className="hidden sm:flex w-1/2 items-center justify-center">
           <img src={illustration} alt="ilustração" className="w-3/4" />
        </div>
 
       <div className={`hidden sm:block w-px my-16 ${darkMode ? 'bg-gray-600' : 'bg-primary'}`}></div>
 
-      <div className="w-full sm:w-1/2 flex flex-col items-center justify-start pt-22 px-20"> 
-        <h1 className={`text-5xl font-bold mb-12 ${darkMode ? 'text-primary-dark-mode' : 'text-primary-dark'}`}>uTask 3.0</h1>
+      <div className="w-full sm:w-1/2 flex flex-col items-center justify-start pt-10 sm:pt-22 px-6 sm:px-20">
+        <h1 className={`text-4xl sm:text-5xl font-bold mb-8 sm:mb-12 ${darkMode ? 'text-primary-dark-mode' : 'text-primary-dark'}`}>uTask 3.0</h1>
         
         <div className="flex flex-col w-full max-w-md">   
           <label className={`text-sm mb-1 font-poppins font-normal ${darkMode ? 'text-white' : 'text-gray-900'}`}>E-mail</label>
